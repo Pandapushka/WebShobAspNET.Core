8e362eed56335d0137a874ac2c976ac4f6ca986b
Никита Андреев <andreev.nik.d@yandex.ru>
2025-03-16 16:00:27 +0300

    рабают с авторизацией

--- WebShobGleb/Controllers/OrderController.cs
+++ WebShobGleb/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopDB.Models;
 using WebShobGleb.Const;
 using WebShobGleb.Models;
 using WebShobGleb.Servises;
@@ -8,31 +10,35 @@ namespace WebShobGleb.Controllers
     public class OrderController : Controller
     {
         private readonly IOrderService _orderService;
+        private readonly UserManager<User> _userManager;
 
-        public OrderController(IOrderService orderService)
+        public OrderController(IOrderService orderService, UserManager<User> userManager)
         {
             _orderService = orderService;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
             // Получаем модель заказа для текущего пользователя
-            var orderVM = _orderService.GetOrderVMForUser(Constants.UserId);
+            var userId = _userManager.GetUserId(User);
+            var orderVM = _orderService.GetOrderVMForUser(userId);
             return View(orderVM);
         }
 
         [HttpPost]
         public IActionResult Make(OrderVM orderVM)
         {
+            var userId = _userManager.GetUserId(User);
             if (!ModelState.IsValid)
             {
                 // Если модель недействительна, обновляем список товаров в модели
-                orderVM = _orderService.RebuildOrderVM(orderVM, Constants.UserId);
+                orderVM = _orderService.RebuildOrderVM(orderVM, userId);
                 return View("Index", orderVM);
             }
 
             // Если модель действительна, создаем заказ
-            _orderService.CreateOrder(orderVM, Constants.UserId);
+            _orderService.CreateOrder(orderVM, userId);
             return View("Success", orderVM);
         }
     }
